4d408858b350bb853b67d86fd481f2fef45856df
Romain Tello <romain.tello@gmail.com>
2020-01-27 15:08:41 +0100

    Add comments

--- Router.php
+++ Router.php
@@ -1,14 +1,22 @@
 <?php
 
+/*
+* Route
+* Classe destinée à contenir les informations de chaque route existante dans l'application
+*/
 class Route {
+    // Nom du contrôleur à invoquer lorsque cette route est appelée
     private $controllerName;
+    // Nom de la méthode à invoquer lorsque cette route est appelée
     private $methodName;
 
+    // Le constructeur permet d'initialiser les propriétés de l'objet
     public function __construct($controllerName, $methodName) {
         $this->controllerName = $controllerName;
         $this->methodName = $methodName;
     }
 
+    // Getters, permettent d'avoir accès aux propriétés privées
     public function getControllerName() {
         return $this->controllerName;
     }
@@ -18,24 +26,40 @@ class Route {
     }
 }
 
+/*
+* Router
+* Classe permettant d'appeler les routes de l'application
+*/
 class Router {
+    // Liste des routes existant dans l'application
     private $routes;
-    
+
+    // Le constructeur permet d'initialiser les routes à partir du fichier de configuration
     public function __construct() {
+        // Lit le fichier de configuration
         $routerData = parse_ini_file('./router.ini');
 
+        // Initialise la liste des routes
         $this->routes = [];
+        // Pour chaque route dans le fichier de configuration
         foreach ($routerData['routes'] as $key => $routeData) {
+            // Sépare le nom du contrôleur et le nom de la méthode
             $routeProperties = explode('#', $routeData);
+            // Crée un nouvel objet Route à partir de ces propriétés
             $route = new Route($routeProperties[0], $routeProperties[1]);
+            // Ajoute la route associée à la clé correspondante
             $this->routes[$key] = $route;
         }
     }
     
+    // Méthode permettant de faire correspondre un nom de route à un objet Route
     public function match($route) {
+        // Si le nom de la route existe dans la liste
         if (isset($this->routes[$route])) {
+            // Renvoie l'objet Route correspondant
             return $this->routes[$route];
         }
+        // Sinon, renvoie null
         return null;
     }
 }
--- index.php
+++ index.php
@@ -1,23 +1,33 @@
 <?php
 
+// Imports
 require_once('./Router.php');
 require_once('./controllers/MainController.php');
 require_once('./controllers/ContactController.php');
 
+// Crée un nouveau routeur
 $router = new Router;
+
+// Si _url n'a pas de valeur, c'est qu'on est sur la page d'accueil
 if (isset($_GET['_url'])) {
-    $route = $_GET['_url'];
+    $routeName = $_GET['_url'];
 } else {
-    $route = 0;
+    $routeName = 0;
 }
-$routeInfo = $router->match($route);
+// Demander au routeur de trouver l'objet Route correspondant au nom demandé
+$route = $router->match($routeName);
 
-if (is_null($routeInfo)) {
+// Si aucune route n'a été trouvée
+if (is_null($route)) {
+    // Renvoyer la page d'erreur 404
     echo 'Cette page n\'existe pas (encore)';
+// Sinon
 } else {
-    $controllerName = $routeInfo->getControllerName();
-    $methodName = $routeInfo->getMethodName();
+    $controllerName = $route->getControllerName();
+    $methodName = $route->getMethodName();
 
+    // Crée un nouveau contrôleur du nom indiqué par la Route
     $controller = new $controllerName;
+    // Invoque la méthode de ce contrôleur du nom indiqué par la Route
     $controller->$methodName();
 }

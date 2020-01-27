04ce03a50685e6d224672f6dfcf78ff16466f86b
Romain Tello <romain.tello@gmail.com>
2020-01-27 11:20:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# s07-j2ee-php-mvc
+ Application MVC en PHP pur pour comprendre les concepts derrière J2EE
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('./views/head.php') ?>
+
+    <div>
+        Bonjour <?php echo $city ?>!
+    </div>
+
+</body>
+</html>
+<?php $city = 'Montpellier' ?>

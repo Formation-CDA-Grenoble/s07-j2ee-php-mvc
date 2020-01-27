f239ba771016aaa78cabfb38db2c274ae940810b
Romain Tello <romain.tello@gmail.com>
2020-01-27 14:46:26 +0100

    Add router.ini file

--- Router.php
+++ Router.php
@@ -1,14 +1,35 @@
 <?php
 
+class Route {
+    private $controllerName;
+    private $methodName;
+
+    public function __construct($controllerName, $methodName) {
+        $this->controllerName = $controllerName;
+        $this->methodName = $methodName;
+    }
+
+    public function getControllerName() {
+        return $this->controllerName;
+    }
+
+    public function getMethodName() {
+        return $this->methodName;
+    }
+}
+
 class Router {
     private $routes;
     
     public function __construct() {
-        $this->routes = [
-            null => ['MainController', 'show'],
-            '/contact' => ['ContactController', 'show'],
-            '/blog' => ['BlogController', 'show'],
-        ]; 
+        $routerData = parse_ini_file('./router.ini');
+
+        $this->routes = [];
+        foreach ($routerData['routes'] as $key => $routeData) {
+            $routeProperties = explode('#', $routeData);
+            $route = new Route($routeProperties[0], $routeProperties[1]);
+            $this->routes[$key] = $route;
+        }
     }
     
     public function match($route) {
--- index.php
+++ index.php
@@ -8,15 +8,15 @@ $router = new Router;
 if (isset($_GET['_url'])) {
     $route = $_GET['_url'];
 } else {
-    $route = null;
+    $route = 0;
 }
 $routeInfo = $router->match($route);
 
 if (is_null($routeInfo)) {
     echo 'Cette page n\'existe pas (encore)';
 } else {
-    $controllerName = $routeInfo[0];
-    $methodName = $routeInfo[1];
+    $controllerName = $routeInfo->getControllerName();
+    $methodName = $routeInfo->getMethodName();
 
     $controller = new $controllerName;
     $controller->$methodName();
